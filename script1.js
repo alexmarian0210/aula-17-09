124c407741219e83da18b633fa8d7937ab40ca91
alexmarian0210 <alex_marian@estudante.sesisenai.org.b>
2025-09-17 21:18:36 -0300

    first commit

---
+++ script1.js
@@ -0,0 +1,7 @@
+// Complete o código:
+let meuNome = "Alex";
+let minhaIdade = 21;
+let minhaCidade = "Imbuia - SC";
+
+// Exiba as informações usando template literals
+console.log(`Meu nome é ${meuNome}, tenho ${minhaIdade} anos e moro em ${minhaCidade}.`);
---
+++ script5.js
@@ -0,0 +1,9 @@
+// Crie um array com 5 frutas
+let frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"];
+
+// Acesse e exiba o primeiro e último elemento
+console.log("Primeira fruta:", frutas[0]);
+console.log("Última fruta:", frutas[frutas.length - 1]);
+
+// Exiba o tamanho do array
+console.log("Total de frutas:", frutas.length);
